d69b849cf9c096f0e2f3e30b643de08bf986de47
AaronMeighoo <aaronmeighoo1@gmail.com>
2025-01-26 18:28:41 -0600

    (feat) log results in readable format in 'change' fn

--- change.js
+++ change.js
@@ -1,7 +1,7 @@
 function change(amt) {
   // assume amt is a dollar amount in the format DD.cc where D is dollars and c is coins
   // e.g. 14.56
-  amt *= 100; // makes the amt an integer for easy readability and simpler-looking calculations
+  amt *= 100; // converts dollars to pennies. e.g. $30 => 3000 pennies
   let coins = {
     c25: 0,
     c10: 0,
@@ -27,10 +27,23 @@ function change(amt) {
   //   coins.c5 = Math.floor(amt / 5);
   //   coins.c1 = Math.floor(amt % 5);
 
-  console.log(coins);
+  //   console.log(coins);
+
+  console.log(
+    `Give: ${coins.c25 === 0 ? `` : `\n${coins.c25} 25c pieces`}${
+      coins.c10 === 0 ? `` : `\n${coins.c10} 10c pieces`
+    }${coins.c5 === 0 ? `` : `\n${coins.c5} 5c pieces`}${
+      coins.c1 === 0 ? `` : `\n${coins.c1} 1c pieces`
+    }\n`
+  ); // outputs the result in a readable, concise format
+  // console.log(
+  //   `Give:\n${coins.c25} 25c pieces,\n${coins.c10} 10c pieces,\n${coins.c5} 5c pieces,\n${coins.c1} 1c pieces\n`
+  // );
+
+  //   coins.c10 === 0 ? null : console.log(coins.c25);
 }
 
-change(1.01); // { c25: 4,  c10: 0, c5: 0, c1: 1 }
+change(1.01); // { c25: 4, c10: 0, c5: 0, c1: 1 }
 change(2.56); // { c25: 10, c10: 0, c5: 1, c1: 1 }
-change(0.43); // { c25: 1,  c10: 1, c5: 1, c1: 3 }
-change(43.98); // { c25: 1,  c10: 1, c5: 1, c1: 3 }
+change(0.43); // { c25: 1, c10: 1, c5: 1, c1: 3 }
+change(43.98); // { c25: 175, c10: 2, c5: 1, c1: 3 }
